29d0973aeac5ab69fbbc2b58053c3dacb2a3e9b5
Dmitry Starina <dimastarina@gmail.com>
2021-08-17 22:00:34 +0300

    add eslint rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import "./App.css";
+import './App.css';
 
-function App() {
+function App () {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Test</h1>
     </div>
   );
